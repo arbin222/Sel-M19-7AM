42e320e37c08f1e83a88237a71788f2481688067
arbin <arbintaj124@gmail.com>
2026-02-27 07:43:37 +0530

    using for loop and func checking even or odd

--- learning.py
+++ learning.py
@@ -1,5 +1,9 @@
-a=int("enter number: ")
-if a%2==0:
-    print("even")
-else:
-    print("odd")
+def even_odd(start,end):
+    for i in range(start,end):
+        a=int("enter number: ")
+        if a%2==0:
+              print("even")
+        else:
+              print("odd")
+
+even_odd(10,25)
